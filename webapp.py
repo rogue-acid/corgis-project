a6db9b214efc1c087b7b70939c269d7ae1a98092
rogue-acid <galyakrinsky@gmail.com>
2025-11-05 12:46:22 -0800

    Feat: add working graph and dropdown question

--- webapp.py
+++ webapp.py
@@ -4,6 +4,8 @@ from markupsafe import Markup
 import os
 import json
 
+
+
 app = Flask(__name__)
 
 
@@ -22,7 +24,8 @@ def render_page1():
     
 @app.route("/p2")
 def render_page2():
-    return render_template('page2.html')
+    magnitudes = magnitude_over_time()
+    return render_template('page2.html', magnitudes=magnitudes)
 
 
 
@@ -37,12 +40,12 @@ def render_fact():
     earthquake_locations = get_earthquake_locations()
     location = request.args.get('location')
     highest_mag_in_location = highest_magnitude_earthquake(location)
-    fact = "In " + location + " the highest magnitude earthquake to happen is " + highest_mag_in_location + "."
+    fact = "In " + location + " the highest magnitude earthquake to happen is " + str(highest_mag_in_location) + "."
     return render_template('page1.html', earthquake_options=earthquake_locations, funFact=fact)
     
     
 def get_earthquake_locations():
-    """Return the html code for the drop down menu.  Each option is a state abbreviation from the demographic data."""
+    """returns the locations of all the earthquakes."""
     with open('earthquakes.json') as earthquake_data:
         earthquakes = json.load(earthquake_data)
     locations=[]
@@ -56,16 +59,26 @@ def get_earthquake_locations():
     
     
 def highest_magnitude_earthquake(location):
-    """Return the name of a county in the given state with the highest percent of under 18 year olds."""
+    """Returns the highest magnitude earthquake to happen in that location."""
     with open('earthquakes.json') as earthquake_data:
         earthquakes = json.load(earthquake_data)
-    highest=0
-    location = ""
-    for earthquake in earthquakes and earthquake["location"]["name"] == location:
-        if earthquake["impact"]["magnitude"] > highest:
+    highest = 0
+    for earthquake in earthquakes:
+        if earthquake["location"]["name"] == location and earthquake["impact"]["magnitude"] > highest:
             highest = earthquake["impact"]["magnitude"]
-            location = earthquake["location"]["name"]
+            
     return highest
+    
+def magnitude_over_time():
+    """ makes the graph data """
+    with open('earthquakes.json') as earthquake_data:
+        earthquakes = json.load(earthquake_data)
+        
+    earthquake_mags = []
+    for earthquake in earthquakes:
+        earthquake_mags.append({"x": earthquake["time"]["epoch"], "y": earthquake["impact"]["magnitude"]})
+        
+    return Markup(earthquake_mags)
  
  
     
